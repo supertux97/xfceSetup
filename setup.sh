4bd74296d8e915f9356eb50179739cc0735fdf9d
erlend <erlend>
2020-09-15 21:38:08 +0200

    update filses

--- setup.sh
+++ setup.sh
@@ -1,7 +1,16 @@
 cp .bashrc ~/.bashrc
-cp -r .config/xfce4 ~/.config/xfce4
 timedatectl set-timezone Europe/Oslo
+
+#settings
+xfconf-query -c xsettings -p /Gdk/WindowScalingFactor -s 2
+xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Alt>r" -s resize_window_key
+xfconf-query -c keyboard-layout -p /Default/XkbLayout -s no
+
 xfce4-panel -r
+xfwm4 --replace 2&>/dev/null &
 sudo pacman -Syy --noconfirm
 sudo pacman -S yay perl vim guake tmux nodejs vscode redshift --noconfirm 
+source ~/.bashrc
+touch .bookmarks
+
 nohup redshift -l 59.913868:10.752245 & 2&1>/dev/null
